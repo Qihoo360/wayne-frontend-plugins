b529bec9071b0e55ee000cfbcd6e417b045cea8c
wilhelmguo <shaowei3608@gmail.com>
2018-11-20 19:41:59 +0800

    frontend: if load ApiKeys by config enableApiKeys

--- admin/sidenav/sidenav.component.ts
+++ admin/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../src/app/shared/auth/auth.service";
 
 @Component({
   selector: 'wayne-sidenav',
@@ -8,7 +9,7 @@ import {Component, OnInit} from '@angular/core';
 
 export class SidenavComponent implements OnInit {
 
-  constructor() {
+  constructor(public authService: AuthService) {
   }
   ngOnInit() {
   }
